a1c4fe54aad6f8bdf32386034dac77dc31130f2d
Enzo Arata <35208572+EnzoArata@users.noreply.github.com>
2022-02-15 15:14:46 -0800

    Added commenting

--- scrap.py
+++ scrap.py
@@ -9,6 +9,8 @@ urls = ["https://nces.ed.gov/COLLEGENAVIGATOR/?s=all&sp=4&pg=1", "https://nces.e
 #appendUrl will be used later when ids for every page is found
 appendUrl = "https://nces.ed.gov/COLLEGENAVIGATOR/?s=all&sp=4&pg=1&id="
 collegeUrlList = []
+
+#use page urls to gather and collect all college specific URLS
 for url in urls:
     html_content = requests.get(url).text
     soup = BeautifulSoup(html_content, "lxml")
@@ -22,33 +24,34 @@ for url in urls:
 
             tempString = str(tr.a)
             #Added special case for University of the Virgin Islands-Albert A. Sheen
+            #Get ID of webpage from URL
             if tempString[48] == '>':
                 subString = tempString[41:47]
             else:
                 subString = tempString[41:49]
             collegeUrlList.append(appendUrl+subString)
             
-
+#Set Headers for CSV file and write in first row
 header = ['Name', 'Street', 'City', 'State', 'Zip', 'Phone', 'Website', 'Type', 'Awards', 'Campus Setting', 'Campus Housing', 'Student Population', 'Student to Faculty Ratio']
 with open("data.csv", 'w') as csvfile:
     csvwriter = csv.writer(csvfile)
 
     csvwriter.writerow(header)
 
-
+#Go through all gathered college specific Urls
 for url in collegeUrlList:
     html_content = requests.get(url).text
     soup = BeautifulSoup(html_content, "html.parser")
     myEtree = etree.HTML(str(soup))
     collegeName = soup.find("span", class_="headerlg").text
     collegeName = str(collegeName)
-
+    #Use Xpath to get data that contains complete address
     address = str(myEtree.xpath('//*[@id="RightContent"]/div[4]/div/div[2]/span/text()'))
     street = " "
     city = " "
     state = " "
     zip = " "
-
+    #parse address into smaller segments for data points
     for i in range(2, len(address)):
         if address[i] ==',':
             if street == " ":
@@ -102,12 +105,15 @@ for url in collegeUrlList:
     population = population[21:len(population)]
 
     #get 8th row for student to faculty ratio, if it exists
+    #there is one college without a faculty ratio
     if len(myTable)>7:
         myRow = (myTable.findAll("tr")[7]).text
         ratio = str(myRow)
         ratio = ratio[27:len(ratio)]
     else:
         ratio = "NA"
+        
+    #write new row to table
     with open("data.csv", 'a', newline='') as csvfile:
         csvwriter = csv.writer(csvfile)
 
